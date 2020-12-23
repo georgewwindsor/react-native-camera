3fb33405f19e6de2d30f79dde812138fad8022c6
George Windsor <georgewwindsor@yahoo.com>
2020-12-23 00:31:43 -0800

    ok..

--- android/src/general/java/org/reactnative/camera/tasks/PixelsProcessedAsyncTask.java
+++ android/src/general/java/org/reactnative/camera/tasks/PixelsProcessedAsyncTask.java
@@ -22,45 +22,33 @@ import org.reactnative.frame.RNFrameFactory;
 public class PixelsProcessedAsyncTask extends android.os.AsyncTask<Void, Void, SparseArray<TextBlock>> {
 
   private PixelsProcessedAsyncTaskDelegate mDelegate;
-  private ThemedReactContext mThemedReactContext;
-
   private byte[] mImageData;
   private int mWidth;
   private int mHeight;
-  private int mRotation;
-  private ImageDimensions mImageDimensions;
-  private double mScaleX;
-  private double mScaleY;
-  private int mPaddingLeft;
-  private int mPaddingTop;
+
 
   public PixelsProcessedAsyncTask(
           PixelsProcessedAsyncTaskDelegate delegate,
           //ThemedReactContext themedReactContext,
           byte[] imageData,
           int width,
-          int height,
-          int rotation,
-          float density,
-          int facing,
-          int viewWidth,
-          int viewHeight,
-          int viewHeight,
-          int viewPaddingLeft,
-          int viewPaddingTop
+          int height
+
   ) {
     mDelegate = delegate;
-    mThemedReactContext = themedReactContext;
+    // mThemedReactContext = themedReactContext;
     mImageData = imageData;
     mWidth = width;
     mHeight = height;
-    mRotation = rotation;
-    mImageDimensions = new ImageDimensions(width, height, rotation, facing);
-    mScaleX = (double) (viewWidth) / (mImageDimensions.getWidth() * density);
-    mScaleY = (double) (viewHeight) / (mImageDimensions.getHeight() * density);
-    mPaddingLeft = viewPaddingLeft;
-    mPaddingTop = viewPaddingTop;
+//     mRotation = rotation;
+//     mImageDimensions = new ImageDimensions(width, height, rotation, facing);
+//     mScaleX = (double) (viewWidth) / (mImageDimensions.getWidth() * density);
+//     mScaleY = (double) (viewHeight) / (mImageDimensions.getHeight() * density);
+//     mPaddingLeft = viewPaddingLeft;
+//     mPaddingTop = viewPaddingTop;
+
   }
+
 
   @Override
   protected SparseArray<TextBlock> doInBackground(Void... ignored) {
@@ -69,26 +57,26 @@ public class PixelsProcessedAsyncTask extends android.os.AsyncTask<Void, Void, S
     }
    // mPixelsProcessed = new TextRecognizer.Builder(mThemedReactContext).build();
    // RNFrame frame = RNFrameFactory.buildFrame(mImageData, mWidth, mHeight, mRotation);
-   // return mTextRecognizer.detect(frame.getFrame());
+    return null;
   }
 
   @Override
   protected void onPostExecute(SparseArray<TextBlock> textBlocks) {
     super.onPostExecute(textBlocks);
 
-    if (textBlocks != null) {
-      WritableArray textBlocksList = Arguments.createArray();
-      for (int i = 0; i < textBlocks.size(); ++i) {
-        TextBlock textBlock = textBlocks.valueAt(i);
-        WritableMap serializedTextBlock = serializeText(textBlock);
-        if (mImageDimensions.getFacing() == CameraView.FACING_FRONT) {
-          serializedTextBlock = rotateTextX(serializedTextBlock);
-        }
-        textBlocksList.pushMap(serializedTextBlock);
-      }
-      mDelegate.onPixelProcessedRecognized(textBlocksList);
-    }
-    mDelegate.onPixelProcessedTaskCompleted();
+//    if (textBlocks != null) {
+//      WritableArray textBlocksList = Arguments.createArray();
+//      for (int i = 0; i < textBlocks.size(); ++i) {
+//        TextBlock textBlock = textBlocks.valueAt(i);
+//        WritableMap serializedTextBlock = serializeText(textBlock);
+//        if (mImageDimensions.getFacing() == CameraView.FACING_FRONT) {
+//          serializedTextBlock = rotateTextX(serializedTextBlock);
+//        }
+//        textBlocksList.pushMap(serializedTextBlock);
+//      }
+//      mDelegate.onPixelProcessedRecognized(textBlocksList);
+//    }
+    mDelegate.onPixelsProcessedTaskCompleted();
   }
 
 
